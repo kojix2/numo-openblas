4eab30fc19acdeb70d397b2789556f55db7700e9
yoshoku <yoshoku@outlook.com>
2021-01-10 23:12:02 +0900

    Add output library files for debug

--- lib/numo/openblas.rb
+++ lib/numo/openblas.rb
@@ -5,4 +5,25 @@ require 'numo/linalg/linalg'
 require 'numo/openblas/version'
 require 'numo/openblas/openblas'
 
+puts "#{__dir__}"
+puts File.expand_path("#{__dir__}/../../vendor/lib/")
+Dir.foreach(File.expand_path("#{__dir__}/../../vendor/lib/")) { |f| puts f }
+
+if RbConfig::CONFIG['host_os'] =~ /mswin|msys|mingw|cygwin/
+  module Numo
+    module Linalg
+      module Loader
+        module_function
+
+        def load_openblas(lib_dir)
+          openblas_path = "#{lib_dir}/libopenblas.dll.a"
+          Numo::Linalg::Blas.dlopen(openblas_path)
+          Numo::Linalg::Lapack.dlopen(openblas_path)
+          @@libs = [openblas_path]
+        end
+      end
+    end
+  end
+end
+
 Numo::Linalg::Loader.load_openblas(File.expand_path("#{__dir__}/../../vendor/lib/"))
